5f7743c4bc51e49aa72db86b987df59380447d18
yaoyongfeng <yaoyongfeng7851@dingtalk.com>
2022-11-19 18:29:30 +0800

    修改上传格式

--- src/AttachmentUploader.php
+++ src/AttachmentUploader.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace ailiangkuai\yii2\widgets\webuploader;
 
 use yii\helpers\ArrayHelper;
@@ -14,18 +15,20 @@ class AttachmentUploader extends BaseUploader
 {
     public $columnClass = '\ailiangkuai\yii2\widgets\webuploader\AttachmentColumn';
 
-    function registerAssert(array $options) {
+    function registerAssert(array $options)
+    {
         $options['contentOptions'] = ArrayHelper::remove($options['options'], 'contentOptions', []);
         AttachmentUploaderAsset::register($this->getView());
         $this->getView()->registerJs("$('#{$this->options['id']}').AttachmentUploader(" . Json::encode($options) . ")");
     }
 
-    public function initColumns(array $options) {
-        $contentOptions                      = ArrayHelper::remove($options, 'contentOptions', []);
-        $contentOptions['class']             = ArrayHelper::getValue($contentOptions, 'class', 'uploader-name');
+    public function initColumns(array $options)
+    {
+        $contentOptions = ArrayHelper::remove($options, 'contentOptions', []);
+        $contentOptions['class'] = ArrayHelper::getValue($contentOptions, 'class', 'uploader-name');
         $this->itemOptions['contentOptions'] = $contentOptions;
-        $values                              = [];
-        if(ArrayHelper::getValue($this->clientOptions,'fileNumLimit',0)>1&&!empty($this->value)){
+        $values = [];
+        if (!empty($this->value)) {
             $this->value = Json::decode($this->value);
         }
         if (ArrayHelper::isIndexed($this->value)) {
@@ -38,7 +41,8 @@ class AttachmentUploader extends BaseUploader
         $this->value = $values;
     }
 
-    protected function createColumn($value, $options) {
+    protected function createColumn($value, $options)
+    {
         return \Yii::createObject([
                 'class' => $this->columnClass, 'contentOptions' => $this->itemOptions['contentOptions'], 'removeOptions' => $this->itemOptions['removeOptions'], 'options' => $options, 'helpOptions' => $this->itemOptions['helpOptions'], 'uploader' => $this,
                 'value' => is_array($value) ? $value : Json::decode($value),
--- src/ImageUploader.php
+++ src/ImageUploader.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace ailiangkuai\yii2\widgets\webuploader;
 
 use yii\helpers\ArrayHelper;
@@ -14,27 +15,38 @@ class ImageUploader extends BaseUploader
 {
     public $columnClass = '\ailiangkuai\yii2\widgets\webuploader\ImageColumn';
 
-    function registerAssert(array $options) {
-        $options['imgOptions']    = ArrayHelper::remove($options['options'], 'imgOptions', []);
+    function registerAssert(array $options)
+    {
+        $options['imgOptions'] = ArrayHelper::remove($options['options'], 'imgOptions', []);
         ImageUploaderAsset::register($this->getView());
         $this->getView()->registerJs("$('#{$this->options['id']}').ImageUploader(" . Json::encode($options) . ")");
     }
 
-    public function initColumns(array $options) {
-        $imgOptions                         = ArrayHelper::remove($options, 'imgOptions', []);
-        $imgOptions['width']                = ArrayHelper::getValue($imgOptions, 'width', 110);
-        $imgOptions['height']               = ArrayHelper::getValue($imgOptions, 'height', 110);
-        $this->itemOptions['imgOptions']    = $imgOptions;
-        if(ArrayHelper::getValue($this->clientOptions,'fileNumLimit',0)>1&&!empty($this->value)){
+    public function initColumns(array $options)
+    {
+        $imgOptions = ArrayHelper::remove($options, 'imgOptions', []);
+        $imgOptions['width'] = ArrayHelper::getValue($imgOptions, 'width', 110);
+        $imgOptions['height'] = ArrayHelper::getValue($imgOptions, 'height', 110);
+        $this->itemOptions['imgOptions'] = $imgOptions;
+        if (!empty($this->value)) {
             $this->value = Json::decode($this->value);
         }
-        foreach ($this->value as $index => $value) {
-            $column              = \Yii::createObject([
+        $values = [];
+        if (ArrayHelper::isIndexed($this->value)) {
+            foreach ($this->value as $index => $value) {
+                $values[] = \Yii::createObject([
+                        'class' => $this->columnClass, 'imgOptions' => $imgOptions, 'removeOptions' => $this->itemOptions['removeOptions'], 'options' => $options, 'helpOptions' => $this->itemOptions['helpOptions'], 'uploader' => $this,
+                        'value' => $value,
+                    ]
+                );
+            }
+        } else {
+            $values[] = \Yii::createObject([
                     'class' => $this->columnClass, 'imgOptions' => $imgOptions, 'removeOptions' => $this->itemOptions['removeOptions'], 'options' => $options, 'helpOptions' => $this->itemOptions['helpOptions'], 'uploader' => $this,
-                    'value' => $value,
+                    'value' => $this->value,
                 ]
             );
-            $this->value[$index] = $column;
         }
+        $this->value = $values;
     }
 }
